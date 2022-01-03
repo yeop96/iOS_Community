e3c6fb8ed5e5e93148a34329e522fb02bafbcba4
yeop96 <ekdh6@naver.com>
2022-01-03 23:00:48 +0900

    Initial Commit

---
+++ community/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  community
+//
+//  Created by yeop on 2022/01/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
